d91402337d1096528a84a2c5f0eab129a498dbde
PopoviciMarian <popovicimarian2311@gmail.com>
2020-02-10 18:42:16 +0200

    prezentare

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.blueAccent,
+          // body: SafeArea( ),
+        ),
+      ),
+    );
+  });
+}
